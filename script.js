3548f978a76f8340fdc44570eb0de6dd8d0ce8ae
unknown <luster.clifford@yahoo.com>
2022-10-21 18:45:01 -0700

    updates

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-// Work in progress... Will contain a 'go to top' button
 // Mailing list feature for deals
+
+// Back to top button functionality
+
+const showPx = 100;
+const backToTopButton = document.querySelector('.back-to-top');
+
+const scrollContainer = () => {
+    return document.documentElement || document.body;
+};
+
+document.addEventListener('scroll', () => {
+    if (scrollContainer().scrollTop > showPx) {
+        backToTopButton.classList.remove('hidden');
+    } else {
+        backToTopButton.classList.add('hidden');
+    }
+});
+
+const goToTop = () => {
+    document.body.scrollIntoView();
+}
+
+backToTopButton.addEventListener('click', goToTop);
